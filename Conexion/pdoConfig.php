8b73f4af5dc65a6bac5da275603a5c877e9a9dd9
Jeffry La Rosa <32849319+JEFFRYLR@users.noreply.github.com>
2020-10-05 16:09:56 -0500

    subo conexion

---
+++ Conexion/pdoConfig.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'databasechatbotadvisor.caynk19nrbhz.us-west-2.rds.amazonaws.com';
+    $dbname = 'databasechatbotadvisor';
+    $username = 'adminAdvisor';
+    $password = 'adminAdvisor';
