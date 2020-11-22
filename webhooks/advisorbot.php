a1358cc35bd408fcabc4a6a53a1a274e88e873ac
Jeffry La Rosa <32849319+JEFFRYLR@users.noreply.github.com>
2020-11-22 11:38:59 -0500

    Coloco corchete que faltaba

--- webhooks/advisorbot.php
+++ webhooks/advisorbot.php
@@ -25,7 +25,7 @@ if (intent_recibido("calculadora")){
 	$respuesta =  $fila[1] . " " . $fila[2] . " Telefono:" . $fila[3] . " ";
 }
 	enviar_texto( "Luego de sumar los valores te digo que el resultado es $resultado y $respuesta");
-
+}
 	//enviar_texto( "Luego de sumar los valores te digo que el resultado es $resultado y $entro");
 	
 
